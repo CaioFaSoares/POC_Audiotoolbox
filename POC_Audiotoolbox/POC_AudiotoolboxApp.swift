4404f9330236db4feeffc9b1b9f0139a72eb7560
Caio <caiofasoares@gmail.com>
2022-11-07 09:14:30 -0300

    Initial Commit

---
+++ POC_Audiotoolbox/POC_AudiotoolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_AudiotoolboxApp.swift
+//  POC_Audiotoolbox
+//
+//  Created by Caio Soares on 07/11/22.
+//
+
+import SwiftUI
+
+@main
+struct POC_AudiotoolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
